f2aa0e6f51c6adc85240e536de0ceacecc89c386
sschrs <s_ozarslan07@outlook.com>
2022-09-28 22:35:10 +0300

    AutoSave function is written

--- marker/save.go
+++ marker/save.go
@@ -6,7 +6,26 @@ import (
 	"image/png"
 	"log"
 	"os"
+	"path/filepath"
 )
+
+var Save = map[string]func(img *image.RGBA, path string){
+	".png":  SavePNG,
+	".jpeg": SaveJPEG,
+	".jpg":  SaveJPEG,
+	".JPG":  SaveJPEG,
+	"JPEG":  SaveJPEG,
+	"auto":  AutoSave,
+}
+
+func AutoSave(img *image.RGBA, path string) {
+	ext := filepath.Ext(path)
+	if ext == ".png" {
+		SavePNG(img, path)
+	} else {
+		SaveJPEG(img, path)
+	}
+}
 
 func SavePNG(img *image.RGBA, path string) {
 	f, err := os.Create(path)
