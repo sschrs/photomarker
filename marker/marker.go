2276df93363292ff045e49ae74d1068a6439907e
sschrs <s_ozarslan07@outlook.com>
2022-10-01 12:04:29 +0300

    Added multithread feature for maximum performance

--- marker/marker.go
+++ marker/marker.go
@@ -8,6 +8,8 @@ import (
 	"log"
 	"os"
 	"path/filepath"
+	"sync"
+	"time"
 )
 
 type Marker struct {
@@ -48,37 +50,53 @@ func (marker *Marker) SetSrcFile(path string) {
 }
 
 func (marker *Marker) Merge() {
+	start := time.Now()
+
 	srcImg := Resize(marker.width, marker.height, marker.srcPhoto)
-	for i, photo := range marker.dstPhotos {
-		img := GetImage(photo)
-
-		x := marker.x
-		y := marker.y
-
-		if x < 0 {
-			x = img.Bounds().Size().X + x - srcImg.Bounds().Size().X
-		}
-
-		if y < 0 {
-			y = img.Bounds().Size().Y + y - srcImg.Bounds().Size().Y
-		}
-
-		mask := image.NewUniform(color.Alpha{uint8(float64(255) * marker.opacity)})
-		newImage := image.NewRGBA(image.Rect(0, 0, img.Bounds().Size().X, img.Bounds().Size().Y))
-
-		draw.Draw(newImage, newImage.Bounds(), img, image.Point{0, 0}, draw.Src)
-		draw.DrawMask(newImage, newImage.Bounds(), srcImg, image.Point{-x, -y}, mask, image.Point{-x, -y}, draw.Over)
-
-		if !marker.replace {
-			dirPath := filepath.Dir(photo)
-			os.Chdir(dirPath)
-			if err := os.MkdirAll(fmt.Sprintf(marker.outPath), 0777); err != nil {
-				log.Fatalln(err.Error())
-			}
-			fileName := filepath.Base(photo)
-			Save[marker.format](newImage, fmt.Sprintf("%s/%s", marker.outPath, fileName))
-			fmt.Println(fmt.Sprintf("%d-) %s marked", i+1, fileName))
-		}
-
+	limit := make(chan struct{}, 12)
+	var wg sync.WaitGroup
+	for _, photo := range marker.dstPhotos {
+		wg.Add(1)
+		go func(photo string) {
+			limit <- struct{}{}
+			defer func() { <-limit }()
+			defer wg.Done()
+			marker.Draw(srcImg, photo)
+		}(photo)
 	}
+	wg.Wait()
+	close(limit)
+	end := time.Now()
+	fmt.Println(fmt.Sprintf("%v for %d files", end.Sub(start), len(marker.dstPhotos)))
+}
+
+func (marker *Marker) Draw(srcImg *image.RGBA, photo string) {
+	img := GetImage(photo)
+
+	x := marker.x
+	y := marker.y
+
+	if x < 0 {
+		x = img.Bounds().Size().X + x - srcImg.Bounds().Size().X
+	}
+
+	if y < 0 {
+		y = img.Bounds().Size().Y + y - srcImg.Bounds().Size().Y
+	}
+
+	mask := image.NewUniform(color.Alpha{uint8(float64(255) * marker.opacity)})
+	newImage := image.NewRGBA(image.Rect(0, 0, img.Bounds().Size().X, img.Bounds().Size().Y))
+
+	draw.Draw(newImage, newImage.Bounds(), img, image.Point{0, 0}, draw.Src)
+	draw.DrawMask(newImage, newImage.Bounds(), srcImg, image.Point{-x, -y}, mask, image.Point{-x, -y}, draw.Over)
+
+	dirPath := filepath.Dir(photo)
+	os.Chdir(dirPath)
+	if err := os.MkdirAll(fmt.Sprintf(marker.outPath), 0777); err != nil {
+		log.Fatalln(err.Error())
+	}
+	fileName := filepath.Base(photo)
+	Save[marker.format](newImage, fmt.Sprintf("%s/%s", marker.outPath, fileName))
+	fmt.Println(fmt.Sprintf("%s marked", fileName))
+
 }
